854172b907d529b3ce1520376cd9713486c7ea68
Daniel Garcia <dgarcia@blackphone.ch>
2015-07-07 09:20:29 +0200

    author en rama creditos

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/author', function(req, res) {
-	res.render('author');
+	res.render('author', {});
 });
 
 router.get('/quizes/question', quizController.question);
